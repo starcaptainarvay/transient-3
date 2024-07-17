8dae1fd01b927e048e2e59c74973eabd63632ec9
Avyay Natarajan <avyaytheguitarist@gmail.com>
2024-07-17 03:39:48 -0400

    And the dumbest example for testing of all time

--- main.lua
+++ main.lua
@@ -1,68 +1,45 @@
 package.path = package.path .. ";./workflower/?.lua"
 
 local flower = require('workflower')
-local workflower = require("workflower")
-local b, f = flower.bucket('next')
 
-local function adder(a, b, i)
-    return nil, a + b, b, i + 1
-end
+local renderQueue, renderQueueCell = flower.queue(nil)
 
-local function recursiveAdder(a, b)
-    -- print(string.format('adding %d to %d', b, a))
-    local _next = 'recursiveAdder'
-    if a % 4 == 0 then
-        _next = 'recursiveAdderStorage'
-    elseif a % 7 == 0 then
-        _next = 'done'
-    end
-    return _next, a + b, b
-end
-
-local function recursiveDone(a, b)
-    return nil, a
-end
-
-
-local threesum = flower({
-    'adder1',
-    {'adder1', 'adder2', 'divZero', 'adder3'},
-    adder1 = adder,
-    adder2 = adder,
-    adder3 = adder,
-    error = function(e, results)
-        print("BEEP BEEP BEEP!!! THREESUM ERRIOR!!!", e)
-        print("results", unpack(results))
-    end
+flower({
+    --...
+    --{..., 'renderQueue'}
+    renderQueue = renderQueue
 })
 
-local storage, cell = flower.queue('recursivebucket')
-local storage2, cell2 = flower.bucket('recursiveAdder')
 
-local recursiveAdd = flower({
-    'recursiveAdder',
-    {'recursiveAdder', {'recursiveAdder', 'recursiveAdderStorage'}, {'recursiveAdder', 'done'}},
-    recursiveAdder = recursiveAdder,
-    recursiveAdderStorage = cell,
-    recursivebucket = cell2,
-    done = recursiveDone,
-    error = function(err, ...)
-        print("BEEP BEEP BEEP!!! ERROR OCCURERED IN RECURSIVEADD!!!!", err, ...)
-    end
+local queue, cell = flower.queue(nil)
+
+local render = flower({
+    'print',
+    {'print', 'queue'},
+    print = function(...) return nil, ... end,
+    queue = cell
 })
 
-storage:on("value", function(...)
-    print("size:", storage:size(), "tail:", ...)
-end)
+renderQueueCell(1,2,3)
+renderQueueCell(1,2,3)
+renderQueueCell(1,2,3)
 
--- print(threesum(5, 2, 1))
--- print(recursiveAdd(1, 1))
+render(flower.bucket())
+render(flower.bucket())
+render(renderQueue)
+render(render)
+render(flower.bucket())
+render(flower.bucket())
 
-print(recursiveAdd)
+print()
+print(queue)
 
-for i=1, 3 do
-    recursiveAdd(i, i)
-end
-
-print(storage)
-print(storage2)
+-- function love.load()
+--     print(table.concat({renderQueue, renderQueue, renderQueue}, ""))
+-- end
+-- function love.draw()
+--     -- print(renderQueue)
+--     while #renderQueue > 0 do
+--         render(renderQueue:pop())
+--     end
+-- end
