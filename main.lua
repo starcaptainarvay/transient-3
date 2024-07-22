4f906d3c594da988aae66e5a4e78a1a1c6749859
Avyay Natarajan <avyaytheguitarist@gmail.com>
2024-07-21 21:22:21 -0400

    Actual project setup of main.lua

--- main.lua
+++ main.lua
@@ -1,53 +1,11 @@
-package.path = package.path .. ";./workflower/?.lua"
+local wf = require("workflower")
+local t3 = require("transient")
+local socket = require("socket")
 
-local flower = require('workflower')
+function love.load()
+    
+end
 
-local renderQueue, renderQueueCell = flower.queue(nil)
-
-flower({
-    --...
-    --{..., 'renderQueue'}
-    renderQueue = renderQueue
-})
-
-
-local queue, cell = flower.queue(nil)
-
-local render = flower({
-    'print',
-    {'print', 'queue'},
-    print = function(...) return nil, ... end,
-    queue = cell
-})
-
-renderQueueCell(1,2,3)
-renderQueueCell(1,2,3)
-renderQueueCell(1,2,3)
-
-render(flower.bucket())
-render(flower.bucket())
-render(renderQueue)
-render(render)
-render(flower.bucket())
-render(flower.bucket())
-
-print()
-print(queue)
-
--- function love.load()
---     print(table.concat({renderQueue, renderQueue, renderQueue}, ""))
--- end
--- function love.draw()
---     -- print(renderQueue)
---     while #renderQueue > 0 do
---         render(renderQueue:pop())
---     end
--- end
-
-local o = flower.observable()
-o:on("render", print)
-o:dispatch("render", 1,2,3)
-o:dispatch("render", 1,2,3)
-o:dispatch("render", 2,34)
-o:dispatch("render", 1,2,3)
-o:dispatch("render", 532)
+function love.draw()
+    
+end
