10f32473496eee1905b5448496c9f99f6213446f
Avyay Natarajan <avyaytheguitarist@gmail.com>
2024-07-22 09:23:33 -0400

    Moved all modules inside /modules + started infrastructure for transient execution

--- main.lua
+++ main.lua
@@ -2,10 +2,20 @@ local wf = require("workflower")
 local t3 = require("transient")
 local socket = require("socket")
 
+local renderQueue, renderQueueCell = wf.queue()
+local drawTexture = wf()
+
 function love.load()
-    
+    -- TODO set up transient and workflows
+    t3.start()
+end
+
+function love.update()
+    t3.update() -- Update state of Transient 3 ECS
 end
 
 function love.draw()
-    
+    for renderedObject in renderQueue:consume() do
+        drawTexture(renderedObject)
+    end
 end
