5c59351b28ccc2a08cddce0692c691af7c210437
willybh11 <willhagele@gmail.com>
2024-09-19 00:39:26 -0400

    SHED demo screen bounds/latency test
    Co-authored-by: Avyay Natarajan <starcaptainarvay@users.noreply.github.com>

--- src/systems/RenderSystem.lua
+++ src/systems/RenderSystem.lua
@@ -1,35 +1,40 @@
 local wf = require("workflower")
 local t3 = require("transient")
-local Shader = require("RenderSystem.shader")
-local Vector = require("src.math.Vector")
+local Shader = require("systems.rendering.shader")
+print("shader loaded")
+
+local vector = require("src.math.vector")
+print("vector loaded")
 
 local RenderSystem = t3.system("Render")
-
-local renderQueue, renderQueueCell = wf.queue()
+local renderQueue, renderQueueCell
 
 function RenderSystem:init(component, entity)
     
 end
 
 function RenderSystem:update(component, entity)
-    
+    print('updawg')
 end
 
 function RenderSystem:updateSystem()
     
 end
 
-function RenderSystem.drawTexture()
+function RenderSystem.drawTexture(...)
 
 end
 
 function RenderSystem.createQueue()
+    if not renderQueue then
+       renderQueue, renderQueueCell = wf.queue()
+    end
     return renderQueue, renderQueueCell, RenderSystem.drawTexture
 end
 
 
-local a = Vector.new(3, 3)
-local b = Vector.new(1, 2)
+local a = vector.new(3, 3)
+local b = vector.new(1, 2)
 
 print("a * b", a * b)
 print("a * 5", a * 5)
