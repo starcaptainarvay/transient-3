2ec60f40da2bc85a0c03c4fb9c85b8d62c3b569f
starcaptainarvay <avyaytheguitarist@gmail.com>
2025-04-21 20:12:49 -0400

    serial system

---
+++ src/systems/SerialSystem.lua
@@ -0,0 +1,32 @@
+local t3 = require("transient")
+local wf = require("workflower")
+
+local SerialSystem = t3.system("Serial")
+local PORT = "COM3"
+local BAUD = 115200
+local COMMAND = string.format('plink -serial %s -sercfg %d,8,n,1 -batch', PORT, BAUD)
+
+local serial, serialPush
+
+function SerialSystem:initSystem()
+    serial = io.popen(COMMAND, "r")
+
+    if not serial then
+        error("Failed to open serial port: " .. PORT)
+    end
+
+    SerialSystem.FIFO, serialPush = wf.queue()
+end
+
+function SerialSystem:updateSystem()
+    local char
+
+    for i=1, 256 do
+        char = serial:read(1)
+        if char then
+            serialPush:push(char)
+        else break end
+    end
+end
+
+return SerialSystem
