9ffe22fb6e5f654ee0c3dd20966cf92a91146351
starcaptainarvay <avyaytheguitarist@gmail.com>
2025-04-25 01:32:08 -0400

    patch guards appropriately

--- src/renderables/rect.lua
+++ src/renderables/rect.lua
@@ -3,15 +3,18 @@ local function rect(size, pos, line_width, line_color, fill_color)
 
     if fill_color then
         love.graphics.setColor(unpack(fill_color))
-        love.graphics.rectangle("fill", pos.x, pos.y, size.x, size.y)
     end
 
-    if line_color then
-        love.graphics.setColor(unpack(line_color))
-    end
+    love.graphics.rectangle("fill", pos.x, pos.y, size.x, size.y)
 
-    love.graphics.setLineWidth(line_width)
-    love.graphics.rectangle("line", pos.x, pos.y, size.x, size.y)
+    if line_width then
+        if line_color then
+            love.graphics.setColor(unpack(line_color))
+        end
+
+        love.graphics.setLineWidth(line_width)
+        love.graphics.rectangle("line", pos.x, pos.y, size.x, size.y)
+    end
 end
 
 return rect
