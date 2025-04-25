1c5c82e492a7ceac1a84220ff5f93b7aa6f2bf74
starcaptainarvay <avyaytheguitarist@gmail.com>
2025-04-25 01:39:44 -0400

    only fill if fill provided

--- src/renderables/rect.lua
+++ src/renderables/rect.lua
@@ -3,9 +3,8 @@ local function rect(size, pos, line_width, line_color, fill_color)
 
     if fill_color then
         love.graphics.setColor(unpack(fill_color))
+        love.graphics.rectangle("fill", pos.x, pos.y, size.x, size.y)
     end
-
-    love.graphics.rectangle("fill", pos.x, pos.y, size.x, size.y)
 
     if line_width then
         if line_color then
