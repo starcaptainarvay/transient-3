8071d9a26448a535f039d97cefa4b483aaf8ca23
starcaptainarvay <avyaytheguitarist@gmail.com>
2025-04-25 03:17:28 -0400

    update renderables

--- src/renderables/init.lua
+++ src/renderables/init.lua
@@ -1,6 +1,8 @@
 local RENDERABLES = {
     rect = require("src.renderables.rect"),
-    point = require("src.renderables.point")
+    point = require("src.renderables.point"),
+    -- canvas = require("src.renderables.canvas"),
+    particle = require("src.renderables.particle"),
 }
 
 return RENDERABLES
