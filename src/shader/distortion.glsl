2a7e19eaf337acae7761f0eee8771da4e6166a8a
starcaptainarvay <avyaytheguitarist@gmail.com>
2025-04-26 03:03:57 -0400

    bring down emissiveness for particles

--- src/shader/distortion.glsl
+++ src/shader/distortion.glsl
@@ -73,6 +73,6 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) 
 
     vec4 noiseColor = noise_effect(color, texture, texture_coords, screen_coords);
     // Return the distorted color
-    return distortedColor * color * noiseColor * 3;
+    return distortedColor * color * noiseColor * 1.2;
 }
 
